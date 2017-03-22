add318203780b4aae72649357e4f2a07b927f8ae
clarion <clarion@cobblehill>
2017-03-22 14:54:40 -0400

    Retrying commit :(
    Former-commit-id: ef77e49f43d0e634f8488e3b3f8fd29d26bf84d0

--- runmd.sh
+++ runmd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #rm equil_all.lammpstrj
-mpiexec -np 4 ~/Programs/lmp_protein/src/lmp_openmpi -in in.equil
+mpiexec -np 2 ~/Programs/lmp_protein/src/lmp_openmpi -in in.equil
 
 natoms=$( head -n 4 poly.lammpstrj | tail -n 1 )
 nlines=$( echo "$natoms + 9" | bc )
